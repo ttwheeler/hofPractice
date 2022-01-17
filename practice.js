c6810700e71660cd2c42a59009d04ce48792c466
Tom Wheeler <ttwheelerb@gmail.com>
2022-01-17 12:05:12 -0600

    Finished all HOF exercises

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // This repo is optional extra practice to use the underscore functions.
-// Here we'll be writing new functions, but these functions will use 
-// the underscore functions within them. 
+// Here we'll be writing new functions, but these functions will use
+// the underscore functions within them.
 
 /*
  *
@@ -22,7 +22,15 @@ var moreFruits = function(fruits) {
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function(numbers) {
+  var count = 0;
 
+  _.each(numbers, function (number, index, collection) {
+    if (number % 5 === 0) {
+      count ++;
+    }
+  });
+
+  return count;
 };
 
 /*
@@ -34,17 +42,24 @@ var multiplesOfFive = function(numbers) {
 // use _.filter to return the fruits array with only the desired fruit.
 var onlyOneFruit = function(fruits, targetFruit) {
 
+  return _.filter(fruits, function (fruit) {
+    return fruit === targetFruit;
+  });
 };
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function(fruits, letter) {
-
+  return _.filter(fruits, function (fruit) {
+    return fruit[0] === letter;
+  });
 };
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function(desserts) {
-
+  return _.filter(desserts, function (dessert) {
+    return dessert.type === 'cookie';
+  });
 };
 
 /*
@@ -55,12 +70,21 @@ var cookiesOnly = function(desserts) {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-  
+
+  return _.reduce(products, function (sum, product) {
+    return sum + parseFloat(product.price.slice(1));
+  }, 0);
+
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function(desserts) {
+
+  return _.reduce(desserts, function (accum, dessert) {
+    accum[dessert.type] ? accum[dessert.type] ++ : accum[dessert.type] = 1;
+    return accum;
+  }, {});
 
 };
 
@@ -68,13 +92,24 @@ var dessertCategories = function(desserts) {
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function(movies) {
-  
+
+  return _.reduce(movies, function (movieList, movie) {
+    if (movie.releaseYear >= 1990 && movie.releaseYear <= 2000) {
+      movieList.push(movie.title);
+    }
+    return movieList;
+  }, []);
+
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
 var movieNight = function(movies, timeLimit) {
+
+  return _.reduce(movies, function (isShorter, movie) {
+    return movie.runtime < timeLimit ? true : isShorter;
+  }, false);
 
 };
 
@@ -87,14 +122,17 @@ var movieNight = function(movies, timeLimit) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function(fruits) {
-
+  return _.map(fruits, function (fruit) { return fruit.toUpperCase(); });
 };
 
 // given an array of dessert objects, return a new array of objects
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function(desserts) {
-
+  return _.map(desserts, function (dessert) {
+    dessert.glutenFree = dessert.ingredients.includes('flour') ? false : true;
+    return dessert;
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -102,7 +140,7 @@ var glutenFree = function(desserts) {
 //
 // having trouble with decimals? check out this article:
 // http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
-// 
+//
 /*
 
  example output:
@@ -111,12 +149,18 @@ var glutenFree = function(desserts) {
     {
       id: 1,
       product: 'Olive Oil',
-      price: '$12.1', 
+      price: '$12.1',
       salePrice: '$9.61'
     }
   ];
 
 */
 var applyCoupon = function(groceries, coupon) {
+  var saleMultiplier = 1.0 - coupon;
+
+  return _.map(groceries, function (item) {
+    item.salePrice = '$' + parseFloat(item.price.slice(1) * saleMultiplier).toFixed(2);
+    return item;
+  });
 
 };
